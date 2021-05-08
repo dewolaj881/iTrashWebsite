55fe0a3a568318626414c8cfb4d284f400ea516d
dewolaj881 <dewolaj881@gmail.com>
2021-05-09 01:07:15 +0200

    feat: showcase image fixed under navbar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import 'tailwindcss/tailwind.css'
 import Nav from "../components/Nav";
+import Showcase from "../components/Showcase"
 
 export default function Home() {
   return (
 
-    <div>
+    <div className="fixed-showcase">
       <Nav></Nav>
+      <Showcase></Showcase>
     </div>
   )
 }
