be8906abcef1ebb2975cbc4d08894a25cfedcfd5
dewolaj881 <dewolaj881@gmail.com>
2021-05-18 21:01:18 +0200

    remove: useless imports

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
+function Showcase() {
   return (
     <section>
       <div>
-        <p>somting</p>
         <img src="../ShowcaseImage.jpg" alt="" />
       </div>
     </section>
   );
 }
 
-export default Nav;
+export default Showcase;
